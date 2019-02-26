6ed3c661d1591d9c9d7a8477cbc508f3663bfb41
Dukens Thelemaque <dukenst2006@gmail.com>
2019-02-26 09:53:56 -0500

    Fix menu issue on mobile

--- resources/js/pages/profile/Index.vue
+++ resources/js/pages/profile/Index.vue
@@ -1,14 +1,16 @@
 <template>
 <div class="profile">
     <!--navbar-->
-        <div>
-            <navbar></navbar>
-        </div>
-        <!--end navbar-->
+    <div>
+        <navbar></navbar>
+    </div>
+    <!--end navbar-->
     <div class="container-fluid" style="background: #ffffff">
-    <div class="row profile">
-		<div class="col-md-3">
-			<div class="profile-sidebar">
+        <div class="d-flex" id="wrapper">
+
+    <!-- Sidebar -->
+    <div id="sidebar-wrapper">
+    <div class="profile-sidebar">
 				<!-- SIDEBAR USERPIC -->
 				<div class="text-center">
 					<img src=" /img/profile/profile.png" class="img-responsive center" alt="">
@@ -53,32 +55,102 @@
 				</div>
 				<!-- END MENU -->
 			</div>
-		</div>
-        <!-- <div class="vertical-divider d-none d-sm-block"></div> -->
-		<div class="col-md-9">
-            <div class="profile-content">
-			    <h2 class="welcome">Welcome to Bibmark</h2>
-                <h5 class="content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure tempora numquam eum quod quia.</h5>
-                <hr class="content-divider">
-            </div>
-		</div>
-	</div>
-</div>
+    </div>
+    <!-- /#sidebar-wrapper -->
 
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+        <button type="button" id="menu-toggle" class="btn btn-warning btn-sm">
+            <span class="navbar-toggler-icon"><i style="font-size:15px" aria-hidden="true" class="fa fa-bars"></i></span>
+        </button>
+      <div class="container-fluid">
+        <div class="profile-content">
+            <h2 class="welcome">Welcome to Bibmark</h2>
+            <h5 class="content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure tempora numquam eum quod quia.</h5>
+            <hr class="content-divider">
+        </div>
+      </div>
+    </div>
+    <!-- /#page-content-wrapper -->
+
+  </div>
+  <!-- /#wrapper -->
+    </div>
+    <home-footer></home-footer>
 </div>
 </template>
 
-<script src="./Profile.js"></script>
+<script src="./Profile.js">
+export default {
+        components : {  },
+        data() {
+            return {
+            };
+        },
+        mounted(){
+        }
+}
+
+</script>
 
 <style lang="scss" scoped>
 
+#sidebar-wrapper {
+    min-height: 100vh;
+    margin-left: -15rem;
+    -webkit-transition: margin .25s ease-out;
+    -moz-transition: margin .25s ease-out;
+    -o-transition: margin .25s ease-out;
+    transition: margin .25s ease-out;
+}
+
+#sidebar-wrapper .sidebar-heading {
+    padding: 0.875rem 1.25rem;
+    font-size: 1.2rem;
+}
+
+#sidebar-wrapper .list-group {
+    width: 15rem;
+}
+
+#page-content-wrapper {
+    min-width: 100vw;
+}
+
+#wrapper.toggled #sidebar-wrapper {
+    margin-left: 0;
+}
+
+@media (min-width: 768px) {
+    #sidebar-wrapper {
+        margin-left: 0;
+    }
+
+    #page-content-wrapper {
+        min-width: 0;
+        width: 100%;
+    }
+
+    #wrapper.toggled #sidebar-wrapper {
+        margin-left: -15rem;
+    }
+}
+
 
 @media (min-width: 992px) { 
-    .profile-sidebar{
+    #menu-toggle{
+        display: none;
+    }
+}
+
+#menu-toggle{
+    margin-left: 25px;
+    margin-top:5px;
+    }
+.profile-sidebar{
     height: 100%;
     border-right: solid 2px #cccccc;
     }
-}
 
 .profile-usertitle-name {
     font-size: 16px;
