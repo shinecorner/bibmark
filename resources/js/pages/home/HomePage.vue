9817e7fe31f6fd8d7bb85e2d1d831020119b6c6c
plagiatmakc <plagiatmakc@gmail.com>
2019-02-27 12:30:12 +0200

    fix css Homepage

--- resources/js/pages/home/HomePage.vue
+++ resources/js/pages/home/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="Home">
+    <div class="main-field">
 
         <!--navbar-->
         <div style="width: 100vw">
@@ -14,31 +14,34 @@
         <!-- end section 1 slider -->
 
         <!-- section 2 placeholder -->
-        <div class="Technology-x-Design textStyle">Technology x Design x Sponsorship</div>
-        <div class="place-holder-image">
+        <div class="placeholder-text default-text-style">Technology x Design x Sponsorship</div>
+        <div>
             <img src="img/place-holder-image.jpg" class="w-100">
             <!--<div class="chicagofire">-->
             <!--&lt;!&ndash; place to background image &ndash;&gt;-->
             <!--</div>-->
-            <!--<div class="BostonMarathonBib">-->
+            <!--<div class="boston-marathon-bib">-->
             <!--&lt;!&ndash; place to label image &ndash;&gt;-->
             <!--</div>-->
         </div>
         <!--end section 2 placeholder -->
 
         <!-- section 3 banner-->
-        <div class="bibmark-kaley">
+        <div class="bibmark-banner">
             <!-- png -->
             <div class="image-banner" ></div>
-            <div class="Design-cus textStyle"><span class="text-style-1">Let’s go:&nbsp;</span>Design custom performance gear, get sponsored & never have to wear a race bib again.</div>
+            <div class="banner-text default-text-style">
+                <span class="banner-text-bold">Let’s go:&nbsp;</span>
+                Design custom performance gear, get sponsored & never have to wear a race bib again.
+            </div>
         </div>
         <!-- end section 3 banner-->
 
         <!-- section 4 carousel-->
-        <div class="textStyle" style="padding-top: 5%">
-            <div class="Start-Designing pt-1">
+        <div class="default-text-style pt-xl-5">
+            <div class="section-carousel-main-text pt-2">
                 Start Designing
-                <div class="Choose-your-product">
+                <div class="section-carousel-subtext pt-xl-4 pt-2">
                 Choose your product and start designing your very own Bibmark today!
             </div>
             </div>
@@ -47,19 +50,35 @@
                         ref="slick"
                         :options="slickOptions"
                 >
-                    <a href="javascript:void(0)"><img src="https://cdn.zeplin.io/5c6d748d4074579a2d70d638/assets/2EA0180A-AD65-4917-B33D-515A98A6C317.png" class="w-100" alt=""></a>
-                    <a href="javascript:void(0)"><img src="https://cdn.zeplin.io/5c6d748d4074579a2d70d638/assets/4D841575-F952-4E1D-AFE1-B6DDF0B7D071.png" class="w-100" alt=""></a>
-                    <a href="javascript:void(0)"><img src="https://cdn.zeplin.io/5c6d748d4074579a2d70d638/assets/3D89C00B-0323-46A1-A829-6B4C03D0AC8C.png" class="w-100" alt=""></a>
-                    <a href="javascript:void(0)"><img src="https://cdn.zeplin.io/5c6d748d4074579a2d70d638/assets/42BC15C9-D51F-4A8A-BB26-BA6ABF12957C.png" class="w-100" alt=""></a>
-                    <a href="javascript:void(0)"><img src="https://cdn.zeplin.io/5c6d748d4074579a2d70d638/assets/2EA0180A-AD65-4917-B33D-515A98A6C317.png" class="w-100" alt=""></a>
-                    <a href="javascript:void(0)"><img src="https://cdn.zeplin.io/5c6d748d4074579a2d70d638/assets/4D841575-F952-4E1D-AFE1-B6DDF0B7D071.png" class="w-100" alt=""></a>
-                    <a href="javascript:void(0)"><img src="https://cdn.zeplin.io/5c6d748d4074579a2d70d638/assets/3D89C00B-0323-46A1-A829-6B4C03D0AC8C.png" class="w-100" alt=""></a>
-                    <a href="javascript:void(0)"><img src="https://cdn.zeplin.io/5c6d748d4074579a2d70d638/assets/42BC15C9-D51F-4A8A-BB26-BA6ABF12957C.png" class="w-100" alt=""></a>
+                    <a href="javascript:void(0)">
+                        <img src="https://cdn.zeplin.io/5c6d748d4074579a2d70d638/assets/2EA0180A-AD65-4917-B33D-515A98A6C317.png" class="w-100" alt="">
+                    </a>
+                    <a href="javascript:void(0)">
+                        <img src="https://cdn.zeplin.io/5c6d748d4074579a2d70d638/assets/4D841575-F952-4E1D-AFE1-B6DDF0B7D071.png" class="w-100" alt="">
+                    </a>
+                    <a href="javascript:void(0)">
+                        <img src="https://cdn.zeplin.io/5c6d748d4074579a2d70d638/assets/3D89C00B-0323-46A1-A829-6B4C03D0AC8C.png" class="w-100" alt="">
+                    </a>
+                    <a href="javascript:void(0)">
+                        <img src="https://cdn.zeplin.io/5c6d748d4074579a2d70d638/assets/42BC15C9-D51F-4A8A-BB26-BA6ABF12957C.png" class="w-100" alt="">
+                    </a>
+                    <a href="javascript:void(0)">
+                        <img src="https://cdn.zeplin.io/5c6d748d4074579a2d70d638/assets/2EA0180A-AD65-4917-B33D-515A98A6C317.png" class="w-100" alt="">
+                    </a>
+                    <a href="javascript:void(0)">
+                        <img src="https://cdn.zeplin.io/5c6d748d4074579a2d70d638/assets/4D841575-F952-4E1D-AFE1-B6DDF0B7D071.png" class="w-100" alt="">
+                    </a>
+                    <a href="javascript:void(0)">
+                        <img src="https://cdn.zeplin.io/5c6d748d4074579a2d70d638/assets/3D89C00B-0323-46A1-A829-6B4C03D0AC8C.png" class="w-100" alt="">
+                    </a>
+                    <a href="javascript:void(0)">
+                        <img src="https://cdn.zeplin.io/5c6d748d4074579a2d70d638/assets/42BC15C9-D51F-4A8A-BB26-BA6ABF12957C.png" class="w-100" alt="">
+                    </a>
                 </slick>
-                <div class="btn prev" @click="prev()">
+                <div class="btn carousel-buttons prev-slide" @click="prev()">
                     <i class="fa fa-angle-left"></i>
                 </div>
-                <div class="btn next" @click="next()">
+                <div class="btn carousel-buttons next-slide" @click="next()">
                     <i class="fa fa-angle-right"></i>
                 </div>
             </div>
@@ -142,33 +161,43 @@
             handleAfterChange(event, slick, currentSlide) {
                 console.log('handleAfterChange', event, slick, currentSlide);
             },
+
             handleBeforeChange(event, slick, currentSlide, nextSlide) {
                 console.log('handleBeforeChange', event, slick, currentSlide, nextSlide);
             },
+
             handleBreakpoint(event, slick, breakpoint) {
                 console.log('handleBreakpoint', event, slick, breakpoint);
             },
+
             handleDestroy(event, slick) {
                 console.log('handleDestroy', event, slick);
             },
+
             handleEdge(event, slick, direction) {
                 console.log('handleEdge', event, slick, direction);
             },
+
             handleInit(event, slick) {
                 console.log('handleInit', event, slick);
             },
+
             handleReInit(event, slick) {
                 console.log('handleReInit', event, slick);
             },
+
             handleSetPosition(event, slick) {
                 console.log('handleSetPosition', event, slick);
             },
+
             handleSwipe(event, slick, direction) {
                 console.log('handleSwipe', event, slick, direction);
             },
+
             handleLazyLoaded(event, slick, image, imageSource) {
                 console.log('handleLazyLoaded', event, slick, image, imageSource);
             },
+
             handleLazeLoadError(event, slick, image, imageSource) {
                 console.log('handleLazeLoadError', event, slick, image, imageSource);
             },
@@ -179,33 +208,18 @@
 
 <style scoped>
 
-    .Home {
+    .main-field {
         margin: 0 auto;
         background-color: white;
         width: 100vw;
     }
 
     /* section 2 placeholder */
-    .Technology-x-Design {
-        position: relative;
-        left: 29.53vw;
+    .placeholder-text {
         margin-top: 5.7%;
-        margin-bottom: 1.8%;
-        width: 42.93vw;
-        height: auto;
         font-size: calc(12px + 1.55vw);/*42px*/
         text-align: center;
         color: #444444;
-        /*z-index: 0;*/
-    }
-
-    .place-holder-image {
-        position: relative;
-        margin-bottom: 2.17%;
-        top: 1.17%;
-        width: 100vw;
-        height: auto;
-        object-fit: contain;
     }
 
     .chicagofire {
@@ -216,7 +230,7 @@
         height: auto;
     }
 
-    .BostonMarathonBib {
+    .boston-marathon-bib {
         position: absolute;
         left: 22.55vw;
         top: 32%;
@@ -225,22 +239,20 @@
     }
 
     /*section 3 banner*/
-    .bibmark-kaley {
+    .bibmark-banner {
         position: relative;
-        padding-top: 1px;
         width: 100vw;
-        height: auto;
-        object-fit: contain;
     }
 
-    .image-banner {
+    .bibmark-banner .image-banner {
         background-image: url('https://cdn.zeplin.io/5c6d748d4074579a2d70d638/assets/DCBBCBC8-6341-4DB8-861B-45A66241117D.png');
         background-position: center center;
         background-size: cover;
         z-index: 3;
         height: 50vw;
     }
-    .Design-cus {
+
+    .bibmark-banner .banner-text {
         position: absolute;
         top: 27%;
         left: 40.78vw;
@@ -252,13 +264,13 @@
         z-index: 1;
     }
 
-    .Design-cus .text-style-1 {
+    .banner-text .banner-text-bold {
         font-size: calc(10px + 2.65vw);
         font-weight: 900;
     }
 
     /*fontstyle*/
-    .textStyle {
+    .default-text-style {
         font-family: "SFProDisplay", "San Francisco", sans-serif;
         font-style: normal;
         font-stretch: normal;
@@ -266,23 +278,15 @@
         letter-spacing: normal;
     }
     /*section 4*/
-    .Start-Designing {
-        margin-left: 29.11vw;
-        padding-top: 2.1%;
-        width: 40.93vw;
+    .section-carousel-main-text {
         font-size: calc(12px + 1.56vw);
         font-weight: bold;
         text-align: center;
         color: #444444;
     }
 
-    .Choose-your-product {
-        position: relative;
+    .section-carousel-subtext {
         background-color: white;
-        padding-top: 2%;
-        padding-left: 2.65vw;
-        width: 38.28vw;
-        height: auto;
         font-family: "SFProText", "San Francisco", sans-serif;
         font-size: calc(8px + 0.52vw);
         font-weight: normal;
@@ -291,30 +295,24 @@
     }
 
     /*carousel*/
-    .prev {
+    .carousel-buttons {
         position: absolute;
-        font-size: 72px;
-        font-weight: lighter;
-        left: 0px;
         top: 25%;
-        text-align: center;
         width: 134px;
         height: 149px;
+        font-size: 72px;
+        font-weight: lighter;
+        text-align: center;
         color: #717171;
         z-index: 9;
     }
 
-    .next {
-        position: absolute;
-        top: 25%;
-        font-size: 72px;
-        font-weight: unset;
-        text-align: center;
+    .carousel-buttons + .prev-slide {
+        left: 0px;
+    }
+
+    .carousel-buttons + .next-slide {
         right: 0px;
-        width: 134px;
-        height: 149px;
-        color: #717171;
-        z-index: 9;
     }
 
     .gradient-carousel {
@@ -324,20 +322,19 @@
 
     .gradient-carousel:before {
         content: "";
-
         z-index: 3;
         position: absolute;
         height: 85%;
         width: 100%;
-        background: -moz-linear-gradient(left, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 49%, rgba(255, 255, 255, 1) 100%);
+        background: -moz-linear-gradient(left, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 20%, rgba(255, 255, 255, 0) 80%, rgba(255, 255, 255, 1) 100%);
         /* FF3.6+ */
-        background: -webkit-gradient(linear, left top, right top, color-stop(0%, rgba(255, 255, 255, 1)), color-stop(49%, rgba(255, 255, 255, 0)), color-stop(100%, rgba(255, 255, 255, 1)));
+        background: -webkit-gradient(linear, left top, right top, color-stop(0%, rgba(255, 255, 255, 1)), color-stop(20%, rgba(255, 255, 255, 0)), color-stop(80%, rgba(255, 255, 255, 0)), color-stop(100%, rgba(255, 255, 255, 1)));
         /* Chrome,Safari4+ */
-        background: -webkit-linear-gradient(left, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 49%, rgba(255, 255, 255, 1) 100%);
+        background: -webkit-linear-gradient(left, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 20%, rgba(255, 255, 255, 0) 80%, rgba(255, 255, 255, 1) 100%);
         /* Chrome10+,Safari5.1+ */
-        background: -o-linear-gradient(left, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 49%, rgba(255, 255, 255, 1) 100%);
+        background: -o-linear-gradient(left, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 20%, rgba(255, 255, 255, 0) 80%, rgba(255, 255, 255, 1) 100%);
         /* Opera 11.10+ */
-        background: -ms-linear-gradient(left, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 49%, rgba(255, 255, 255, 1) 100%);
+        background: -ms-linear-gradient(left, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 20%, rgba(255, 255, 255, 0) 80%, rgba(255, 255, 255, 1) 100%);
         /* IE10+ */
         background: linear-gradient(to right, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 20%, rgba(255, 255, 255, 0) 80%, rgba(255, 255, 255, 1) 100%);
         /* W3C */
