c6c8d6aea8f2d6b6d52b5b1f8565923dbdea028e
Igor-carvalho <igorcarv.consult@gmail.com>
2019-02-28 10:03:55 -0300

    [fix] Make the profile page responsive

--- resources/js/pages/profile/Profile.vue
+++ resources/js/pages/profile/Profile.vue
@@ -2,7 +2,7 @@
     <div class="container-fluid1" style="background: #ffffff">
         <div class="d-flex" id="wrapper">
             <!-- Sidebar -->
-            <div id="sidebar-wrapper" >
+            <div id="sidebar-wrapper">
                 <div class="profile-sidebar">
                     <!-- SIDEBAR USERPIC -->
                     <div class="text-center">
@@ -52,12 +52,14 @@
             <!-- Page Content -->
             <div id="page-content-wrapper">
                 <button type="button" id="menu-toggle" class="btn btn-warning btn-sm">
-                    <span class="navbar-toggler-icon"><i style="font-size:15px" aria-hidden="true" class="fa fa-bars"></i></span>
+                    <span class="navbar-toggler-icon"><i style="font-size:15px" aria-hidden="true"
+                                                         class="fa fa-bars"></i></span>
                 </button>
                 <div class="container-fluid1">
                     <div class="profile-content">
                         <h2 class="welcome">Welcome to Bibmark</h2>
-                        <h5 class="content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure tempora numquam eum quod quia.</h5>
+                        <h5 class="content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure tempora
+                            numquam eum quod quia.</h5>
                         <hr class="content-divider">
                     </div>
                 </div>
@@ -70,15 +72,14 @@
 </template>
 
 <script src="./Profile.js">
-export default {
-        components : {  },
+    export default {
+        components: {},
         data() {
-            return {
-            };
+            return {};
         },
-        mounted(){
+        mounted() {
         }
-}
+    }
 
 </script>
 
@@ -89,212 +90,221 @@ export default {
         src: url("/fonts/HelveticaNeueCyr-Bold.eot");
         src: url("/fonts/HelveticaNeueCyr-Bold.woff") format("woff");
     }
+
     @font-face {
         font-family: "SFUIDisplay-Regular";
         src: url("/fonts/SFUIDisplay-Regular.ttf") format('truetype');
     }
+
     @font-face {
         font-family: "SFProText";
         src: url("/fonts/SFUIText-Bold.ttf") format('truetype');
     }
+
     @font-face {
         font-family: "SFProDisplay";
         src: url("/fonts/SourceSansPro-Bold.ttf") format('truetype');
     }
+
     @font-face {
         font-family: "SourceSansPro-Regular";
         src: url("/fonts/SourceSansPro-Regular.ttf") format('truetype');
     }
 
-
-#sidebar-wrapper {
-    min-height: 100vh;
-    margin-left: -15rem;
-    -webkit-transition: margin .25s ease-out;
-    -moz-transition: margin .25s ease-out;
-    -o-transition: margin .25s ease-out;
-    transition: margin .25s ease-out;
-    border-right: solid 2px #cccccc;
-}
-
-#sidebar-wrapper .sidebar-heading {
-    padding: 0.875rem 1.25rem;
-    font-size: 1.2rem;
-}
-
-#sidebar-wrapper .list-group {
-    width: 15rem;
-}
-
-#page-content-wrapper {
-    min-width: 100vw;
-}
-
-#wrapper.toggled #sidebar-wrapper {
-    margin-left: 0;
-}
-
-@media (min-width: 768px) {
-    #menu-toggle{
-        display: none;
-    }
     #sidebar-wrapper {
+        min-height: 100vh;
+        margin-left: -15rem;
+        -webkit-transition: margin .25s ease-out;
+        -moz-transition: margin .25s ease-out;
+        -o-transition: margin .25s ease-out;
+        transition: margin .25s ease-out;
+        border-right: solid 2px #cccccc;
+    }
+
+    #sidebar-wrapper .sidebar-heading {
+        padding: 0.875rem 1.25rem;
+        font-size: 1.2rem;
+    }
+
+    #sidebar-wrapper .list-group {
+        width: 15rem;
+    }
+
+    #page-content-wrapper {
+        min-width: 100vw;
+    }
+
+    #wrapper.toggled #sidebar-wrapper {
         margin-left: 0;
     }
 
-    #page-content-wrapper {
-        min-width: 0;
-        width: 100%;
+    #menu-toggle {
+        margin-left: 25px;
+        margin-top: 5px;
     }
 
-    #wrapper.toggled #sidebar-wrapper {
-        margin-left: -15rem;
-    }
-}
-
-@media (max-width: 768px) {
-    #sidebar-wrapper {
-        margin-left: -310px;
-    }
-
-    #page-content-wrapper {
-        min-width: 0;
-        width: 100%;
-    }
-
-    #wrapper.toggled #sidebar-wrapper {
-        margin-left: -15rem;
-    }
     .profile-sidebar {
-        padding: 0px !important;
+        padding-left: 119px;
+        padding-right: 69px;
     }
-}
 
-
-@media (min-width: 992px) {
-    #menu-toggle{
-        display: none;
+    .profile-usertitle-name {
+        font-size: 16px;
+        font-weight: 500;
+        font-style: normal;
+        font-stretch: normal;
+        line-height: normal;
+        letter-spacing: 2px;
+        text-align: center;
+        color: #000000;
+        margin-top: 44px;
+        margin-bottom: 58px;
+        /*border-bottom: solid 1px #cccccc;*/
+        height: 19px;
+        opacity: 0.9;
+        font-family: SFProDisplay;
     }
-}
 
-#menu-toggle{
-    margin-left: 25px;
-    margin-top:5px;
+    .profile-usermenu ul li a {
+        font-family: HelveticaNeue;
+        font-size: 18px;
+        font-weight: bold;
+        font-style: normal;
+        font-stretch: normal;
+        line-height: normal;
+        letter-spacing: normal;
+        color: #444444;
+        margin: 14px 15px;
     }
-.profile-sidebar{
-    padding-left: 119px;
-    padding-right:69px;
-}
 
-.profile-usertitle-name {
-    font-size: 16px;
-    font-weight: 500;
-    font-style: normal;
-    font-stretch: normal;
-    line-height: normal;
-    letter-spacing: 2px;
-    text-align: center;
-    color: #000000;
-    margin-top: 44px;
-    margin-bottom: 58px;
-    /*border-bottom: solid 1px #cccccc;*/
-    height: 19px;
-    opacity: 0.9;
-    font-family: SFProDisplay;
-}
+    .profile-usermenu ul li a:hover {
+        background-color: #fafcfd;
+        color: #ffc600;
+    }
 
-.profile-usermenu ul li a {
-    font-family: HelveticaNeue;
-    font-size: 18px;
-    font-weight: bold;
-    font-style: normal;
-    font-stretch: normal;
-    line-height: normal;
-    letter-spacing: normal;
-    color: #444444;
-    margin: 14px 15px;
-}
+    .profile-usermenu ul {
+        margin-bottom: 33px;
+    }
 
-.profile-usermenu ul li a:hover {
-    background-color: #fafcfd;
-    color: #ffc600;
-}
-.profile-usermenu ul {
-    margin-bottom: 33px;
-}
-.nav-link {
-    padding: 0px;
-}
-.active {
-    border-bottom: solid 2px #ffc600 !important;
-    padding-bottom: 9px;
-    margin-bottom: 3px !important;
-    width: fit-content;
-}
+    .nav-link {
+        padding: 0px;
+    }
 
-/* Profile Content */
-.profile-content {
-    padding-right: 80px;
-    padding-left: 70px;
-    background: #fff;
-    min-height: 460px;
-}
+    .active {
+        border-bottom: solid 2px #ffc600 !important;
+        padding-bottom: 9px;
+        margin-bottom: 3px !important;
+        width: fit-content;
+    }
 
-.welcome{
-    width: 786px;
-    height: 59px;
-    font-family: SFProDisplay;
-    font-size: 42px;
-    font-weight: bold;
-    font-style: normal;
-    font-stretch: normal;
-    line-height: normal;
-    letter-spacing: normal;
-    color: #444444;
-    margin-bottom: 16px;
-}
+    /* Profile Content */
+    .profile-content {
+        padding-right: 80px;
+        padding-left: 70px;
+        background: #fff;
+        min-height: 460px;
+    }
 
-.dashboard-link{
-    margin-top: 34px;
-    margin-left: 130px;
-    margin-bottom: 275px;
-    height: 22px;
-    font-family: HelveticaNeue;
-    font-size: 18px;
-    font-weight: bold;
-    font-style: normal;
-    font-stretch: normal;
-    line-height: normal;
-    letter-spacing: normal;
-    color: #4a90e2;
-}
-hr.sidebar-divider {
-    width: 100%;
-    border-color: #cccccc;
-    border-width: 2px;
-    /*margin-top: 2rem;*/
-    /*margin-bottom: 1rem;*/
-    /*border-top: 1px solid #cccccc;*/
-    /*border-bottom: 1px solid #cccccc;*/
-}
+    .welcome {
+        font-family: SFProDisplay;
+        font-size: 42px;
+        font-weight: bold;
+        font-style: normal;
+        font-stretch: normal;
+        line-height: normal;
+        letter-spacing: normal;
+        color: #444444;
+        margin-bottom: 16px;
+    }
 
-hr.content-divider{
-	border-top: 1px solid #cccccc;
-}
-.content{
-    margin-bottom: 58px;
-    font-family: SFProText;
-    font-size: 18px;
-    font-weight: normal;
-    font-style: normal;
-    font-stretch: normal;
-    line-height: normal;
-    letter-spacing: normal;
-    color: #000000;
-}
+    .dashboard-link {
+        margin-top: 34px;
+        margin-left: 130px;
+        margin-bottom: 275px;
+        height: 22px;
+        font-family: HelveticaNeue;
+        font-size: 18px;
+        font-weight: bold;
+        font-style: normal;
+        font-stretch: normal;
+        line-height: normal;
+        letter-spacing: normal;
+        color: #4a90e2;
+    }
 
-#wrapper {
-    padding-top: 59px;
-}
+    hr.sidebar-divider {
+        width: 100%;
+        border-color: #cccccc;
+        border-width: 2px;
+        /*margin-top: 2rem;*/
+        /*margin-bottom: 1rem;*/
+        /*border-top: 1px solid #cccccc;*/
+        /*border-bottom: 1px solid #cccccc;*/
+    }
 
+    hr.content-divider {
+        border-top: 1px solid #cccccc;
+    }
+
+    .content {
+        margin-bottom: 58px;
+        font-family: SFProText;
+        font-size: 18px;
+        font-weight: normal;
+        font-style: normal;
+        font-stretch: normal;
+        line-height: normal;
+        letter-spacing: normal;
+        color: #000000;
+    }
+
+    #wrapper {
+        padding-top: 59px;
+    }
+
+    @media (min-width: 768px) {
+        #menu-toggle {
+            display: none;
+        }
+        #sidebar-wrapper {
+            margin-left: 0;
+        }
+
+        #page-content-wrapper {
+            min-width: 0;
+            width: 100%;
+        }
+
+        #wrapper.toggled #sidebar-wrapper {
+            margin-left: -15rem;
+        }
+    }
+
+    @media (max-width: 768px) {
+        .profile-content {
+            padding: 0px 20px !important;
+            text-align: center;
+        }
+        #sidebar-wrapper {
+            margin-left: -310px;
+        }
+
+        #page-content-wrapper {
+            min-width: 0;
+            width: 100%;
+        }
+
+        #wrapper.toggled #sidebar-wrapper {
+            margin-left: -15rem;
+        }
+        .profile-sidebar {
+            padding: 0px !important;
+        }
+    }
+
+    @media (min-width: 992px) {
+        #menu-toggle {
+            display: none;
+        }
+    }
 </style>
