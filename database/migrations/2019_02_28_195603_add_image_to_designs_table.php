979a595941d1823b71ce4224b32e2f8fce3dc273
Phillip Mrzyglocki <pmrzyg.upwork@gmail.com>
2019-03-03 23:02:03 -0500

    update migration

--- database/migrations/2019_02_28_195603_add_image_to_designs_table.php
+++ database/migrations/2019_02_28_195603_add_image_to_designs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddImageToDesignTable extends Migration
+class AddImageToDesignsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class AddImageToDesignTable extends Migration
      */
     public function up()
     {
-        Schema::table('design', function (Blueprint $table) {
+        Schema::table('designs', function (Blueprint $table) {
             $table->string('image');
         });
     }
@@ -25,7 +25,7 @@ class AddImageToDesignTable extends Migration
      */
     public function down()
     {
-        Schema::table('design', function (Blueprint $table) {
+        Schema::table('designs', function (Blueprint $table) {
             $table->dropColumn('image');
         });
     }
--- database/migrations/2019_03_04_033718_add_photo_to_users_table.php
+++ database/migrations/2019_03_04_033718_add_photo_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddPhotoToUserTable extends Migration
+class AddPhotoToUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class AddPhotoToUserTable extends Migration
      */
     public function up()
     {
-        Schema::table('user', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->string('photo')->nullable();
         });
     }
@@ -25,7 +25,7 @@ class AddPhotoToUserTable extends Migration
      */
     public function down()
     {
-        Schema::table('user', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('photo');
         });
     }
